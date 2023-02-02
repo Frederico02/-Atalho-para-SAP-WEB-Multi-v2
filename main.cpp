b3530c66129ec6844ed14006988b656f79b0f21d
Frederico02 <81171707+Frederico02@users.noreply.github.com>
2023-02-02 02:23:00 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//*******************Atalho SAP *******************
+//Desenvolvido por Frederico de Jesus Almeida
+//Analista de Suporte PLENO Multi
+//*******************01/02/2023******************
+
+#include <windows.h>
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+	
+	ShowWindow(GetConsoleWindow(), SW_HIDE); //prompt de comando
+	//system("start chrome.exe -silent -setDefaultBrowser"); TORNAR O GOOGLE PADRAO 
+	//system("start https://s4prd.sap.aws.grupomulti.com.br/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#Shell-ho");
+	system("start https://sts.grupomulti.com.br/adfs/ls/?SAMLRequest=fVFdS8MwFP0rJe9t0nTVEdbCcAqFOYYbPviWpbcukCY1NxH997YdyHyYcJ8O55O7QtmbQaxjONsX%2BIiAIWk2FTkU7UmqRc7SUuX3aQ7QpnLZ3qWQd8tCcV6UkpPkFTxqZyvCM0aSBjFCYzFIG0aI8SJlfLwjKwVbCF6%2BkWQzJmgrw6w6hzCgoBQDZu8%2BDq6PJuhMuT47eSrbDqlBSpIn5xXMHSvSSYMwZe0lov6EX%2BSrNxbFvKci0VvhJGoUVvaAIihxWD9vxdhTDN4Fp5wh9Wpii7m2v9L%2FLx9jwU%2F9Sb1%2FzB%2B2u%2BOCsRW9srr4DmI3apvN3hmtvqcNvQy3rfMsnxHdpt1MFdHiAEp3GlpC60vC31fVPw%3D%3D&RelayState=ouctdbacurqovcrxoreedoaydwoerfytcsstvas&SigAlg=http%3A%2F%2Fwww.w3.org%2F2001%2F04%2Fxmldsig-more%23rsa-sha256&Signature=IV2JLH15PAwKjaaxDvY3kuwv8N1RAJ8I6xnyI1YacllL3J4%2FhrZMuydG4SITe2BFCEEZCOS%2B0o7lXC87XJoM6PN3k0IDGC0D4Uw6aAqQgbGc22H2P9KlvFLNtKTl8Ry9Rt13veJSLb8BS7JmajiLsIWy0yaaHlzX1iLVZoLUxUIhqBlLNAHJjo2hKx4OUCuyoBONmLeb2sOhgNER9G5gKA1OnCmbR8ynMlFBxpvRIfz%2FYCS6Wsa3L5FHJ96SGxiHJ%2F9CyZv74fKS%2Fei2Dqk4C3fkdIIkRum3QN%2B8Gv1AC%2BkBmxReX%2Fh8tPTSAUgNwxjiVtNrH7g%2FTJrQSQxi35wFdITQexjMhw2aKPYDIDJ6fBQNMVl5YSHhsHUUhMJat58wrnTZEdZUFZ7lPHLRRhBWYVpb%2FcEC8xdWWgr23WMrSJzSrGX%2F4cgdSKwCrwFIBNrlOPNJxreM%2BbAZnQRHwyLNcoQq5mj8pgJhoi%2FjVXYLyqyrf5aLuPiUG7eaqX14zgEs");
+
+	return 0;
+}
+
+
